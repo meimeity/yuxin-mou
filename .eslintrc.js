6c3ba0999328b839a95cfa8f3d4132ebfd7328c9
mouyuxin <1977108663@qq.com>
2020-04-19 11:05:10 +0800

    change

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   'extends': [
     'plugin:vue/essential',
     '@vue/standard'
-    
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
